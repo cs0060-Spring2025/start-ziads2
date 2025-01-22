395cdd366bcef968ed0c7ac7e40a4db44296cc6c
ziads2 <165981194+ziads2@users.noreply.github.com>
2025-01-22 09:18:06 -0500

    Update start.py

--- start.py
+++ start.py
@@ -7,5 +7,5 @@ Test Program
 # Once done, commit the changes
 
 
-print ("Hi there, my name is David Prager")
-print ("My GitHub username is -------")
+print ("Hi there, my name is Zhiyuan Yuan")
+print ("My GitHub username is -ziads2")
